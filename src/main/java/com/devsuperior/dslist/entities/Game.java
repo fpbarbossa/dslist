0f3185eb086593644b6a561cf725b2bab6614618
Felipe Barbosa <fpbarbosa@live.com>
2023-11-15 10:32:41 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,22 +21,28 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
-    private String imUrl;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    @Column (columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column (columnDefinition = "TEXT")
     private String longDescription;
     
     public Game(){
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double Score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
-        this.imUrl = imUrl;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
@@ -74,19 +80,27 @@ public class Game {
     }
 
     public String getPlatform() {
-        return platform;
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatform(String platforms) {
+        this.platforms = platforms;
     }
 
-    public String getImUrl() {
-        return imUrl;
+    public Double getScore() {
+        return score;
     }
 
-    public void setImUrl(String imUrl) {
-        this.imUrl = imUrl;
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
